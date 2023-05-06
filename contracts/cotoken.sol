8ca41177595c5f0a451315c9a0e56978f7bf1e5f
Lucasch1 <lucasch2011@gmail.com>
2023-05-06 20:40:56 -0300

    cotokens alteration

--- contracts/cotoken.sol
+++ contracts/cotoken.sol
@@ -6,38 +6,59 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract deployCotoken {
-    address[] public deployedCons;
-
-    function createCotoken(uint minimum) public {
-        address newCotoken  = new cotoken(minimum, msg.sender);
-        deployedCons.push(newCotoken);
+contract deployedCTokens {
+    address[] public deployedTokens;
+    function addNewToken(address _tkAddr) public{
+        deployedTokens.push(_tkAddr);
     }
 
-    function getDeployedCotoken() public view returns (address[]) {
-        return deployedCons;
+    function getDeployedTokens() public view returns(address [] memory) {
+        return deployedTokens;
     }
+    
 }
 
 contract cotoken is ERC20, ERC20Burnable, Ownable {
     
-    mapping(uint256 => uint256) public valorToken;
+    
+    
+    deployedCTokens public Ctokens;
+
+    uint public valorDeUmToken;
     ///Cria variavel para armazenar o enderco atual do contrato cotoken
     address public cotokenAddr;
     
+
+
+    struct token {
+        string name;
+        string symbol;
+        string tipo;
+        string unidadeDeMedida;
+        uint256 supply;
+        uint256 valor;
+        address addr;
+    }
+
+    token public tk;
+
     ///Constroi o token com as funcionalidades padrao do openzeppelin
-    constructor(
-        string memory _nome,
-        string memory _symbol,
-        //string memory _tipo,
-        //string memory _unidadeDeMedida,
-        uint256 _supply,
-        uint256 _valor
-    
-    ) ERC20(_nome, _symbol) {
+    constructor(string memory _nome, string memory _symbol, string memory _tipo, string memory _unidadeDeMedida, uint256 _supply, uint256 _valor, address _CtokensAddr) ERC20(_nome, _symbol) {
         _mint(msg.sender, _supply * 10 ** decimals());
-        valorToken[totalSupply()] = _valor;
-        return address(this);
+        valorDeUmToken = _valor;
+        Ctokens = deployedCTokens(_CtokensAddr);
+        Ctokens.addNewToken(address(this));
+
+        tk = token({
+            name: _nome,
+            symbol: _symbol,
+            tipo: _tipo,
+            unidadeDeMedida: _unidadeDeMedida,
+            supply: _supply,
+            valor: _valor,
+            addr: address(this)
+        });
+
     }
 
     ///Funcao para mintar novos tokens
@@ -46,15 +67,4 @@ contract cotoken is ERC20, ERC20Burnable, Ownable {
         _mint(to, amount);
     }
 
-    ///Funcao para salvar em uma variavel o endereco do cotoken. Como parametro pede o endereco do contrato
-    ///@notice funcao exige gas fees (transact)
-    function setContractaddress( address _contractaddress) public onlyOwner {
-        cotokenAddr = _contractaddress;
-    }
-
-    ///Funcao para aprovar o contrato cotoken a gastar um determinado numero de tokens. Como parametro pede a quantidade
-    ///@notice funcao exige gas fees (transact)
-    function aprovePartyChain(uint _amount) public {
-        approve(cotokenAddr, _amount);
-    }
 }
