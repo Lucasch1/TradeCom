12f6b60a47430de682f86a80681a056c7b00111e
Lucasch1 <lucasch2011@gmail.com>
2023-05-07 08:59:32 -0300

    mudancas

--- contractinstances/btgopc.js
+++ contractinstances/btgopc.js
@@ -1,5 +1,6 @@
-const address = '0xaCd73E091D2D375A7C5492a20F6A4C9B3A7BFFe6'
 
+const address = '0x4793b2FD9039A24c08Ea6851a7bCF3859Af5ed69'
+//0x061Db515C5d3108e558C642Bb22D63ca6Ec73445
 const { abi } = require('../build/contracts/btgOpc.json')
 
 const btgOpcContract = (web3) => {
